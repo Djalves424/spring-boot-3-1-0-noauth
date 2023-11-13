a5f4274533e4e15fa9e9dff30d40ebe3e9db73b4
Diego Alves <djalves424@gmail.com>
2023-11-12 22:13:20 -0300

    Created Project of test

---
+++ src/main/java/com/devsuperior/demo/entities/Role.java
@@ -0,0 +1,57 @@
+package com.devsuperior.demo.entities;
+
+import jakarta.persistence.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_role")
+public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String authority;
+
+    public Role() {
+
+    }
+
+    public Role(Long id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Autowired
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Role role = (Role) o;
+
+        return Objects.equals(authority, role.authority);
+    }
+
+    @Override
+    public int hashCode() {
+        return authority != null ? authority.hashCode() : 0;
+    }
+}
---
+++ src/main/java/com/devsuperior/demo/projections/UserDetailsProjection.java
@@ -0,0 +1,13 @@
+package com.devsuperior.demo.projections;
+
+public interface UserDetailsProjection {
+
+    String getUsername();
+
+    String getPassword();
+
+    Long getRoleId();
+
+    String getAuthority();
+
+}
